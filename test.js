b7a4a6467724b1cbd5a42a57b01da29916251f33
Sindre Sorhus <sindresorhus@gmail.com>
2016-02-15 13:22:33 +0700

    improve tests

--- test.js
+++ test.js
@@ -1,24 +1,10 @@
-import childProcess from 'child_process';
 import test from 'ava';
+import execa from 'execa';
 
-test('main', t => {
-	t.plan(2);
-
-	childProcess.execFile('./cli.js', ['fixture.js'], {
-		cwd: __dirname
-	}, (err, stdout) => {
-		t.ifError(err);
-		t.is(stdout, '  ');
-	});
+test('main', async t => {
+	t.is((await execa('./cli.js', ['fixture.js'])).stdout, '  ');
 });
 
-test('stdin', t => {
-	t.plan(2);
-
-	childProcess.exec('cat fixture.js | ./cli.js', {
-		cwd: __dirname
-	}, (err, stdout) => {
-		t.ifError(err);
-		t.is(stdout, '  ');
-	});
+test('stdin', async t => {
+	t.is((await execa.shell('cat fixture.js | ./cli.js')).stdout, '  ');
 });
